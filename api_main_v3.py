cee6c25915084ffa3a32e5906caca0c48a8d14d0
Alex Lima <caps@MacBook-Pro-de-Alex.local>
2018-06-06 15:19:22 +0200

    modif

--- api_main_v3.py
+++ api_main_v3.py
@@ -1,9 +1,9 @@
 from api_mod_v3 import n_current, registre_s, registre_l, recup_val
 
 x = n_current(('https://api.coinmarketcap.com/v2/ticker/1027?convert=EUR.json','https://api.coinmarketcap.com/v2/ticker/1?convert=EUR.json'),('newstest4.csv','newstest3.csv'))
-l_sc = registre_l()
+l_sc = registre_l('a')
 sc = x.run()
-registre_s(sc)
+registre_s(sc,'a')
 m_stat = x.market_val()
 fin = recup_val(sc, l_sc, m_stat)
 fin.print_info()
